09b92e9acdb417a8ce64779b512cdfe05146bf1d
Luis Felipe Mileo <mileo@kmee.com.br>
2019-09-03 14:16:17 -0300

    [REF] assertEquals to assertEqual
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- tests/test_tools_misc.py
+++ tests/test_tools_misc.py
@@ -12,14 +12,14 @@ from erpbrasil.base.misc import punctuation_rm
 class Tests(TestCase):
 
     def test_punctution_rm(self):
-        self.assertEquals(
+        self.assertEqual(
             punctuation_rm('496.85994.95-7'), '49685994957',
             'The function punctuation_rm failed.')
 
     def test_calc_price_ratio(self):
-        self.assertEquals(
+        self.assertEqual(
             calc_price_ratio(10, 100, 1000), 1.0,
             'The function calc_price_ratio failed.')
-        self.assertEquals(
+        self.assertEqual(
             calc_price_ratio(10, 100, 0), 0,
             'The function calc_price_ratio failed.')
