dd4f93a7b178299693323da57b2ac71e1c147401
Luis Malta <luis.malta@kmee.com.br>
2019-09-04 09:02:54 -0300

    [FIX]validate_ie

--- src/erpbrasil/base/fiscal.py
+++ src/erpbrasil/base/fiscal.py
@@ -32,10 +32,10 @@ PARAMETERS = {
 def validate_ie(uf, inscr_est):
     result = True
     try:
-        validate = getattr(locals(), 'validate_ie_%s' % uf)
+        validate = globals()['validate_ie_%s' % uf]
         if not validate(inscr_est):
             result = False
-    except AttributeError:
+    except KeyError:
         if not validate_ie_param(uf, inscr_est):
             result = False
     return result
