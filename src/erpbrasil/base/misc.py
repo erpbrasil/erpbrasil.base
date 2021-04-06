f2978d9cbb10719b54dc3a9ad0bdf76df6c8c993
Luis Felipe Mileo <mileo@kmee.com.br>
2021-04-05 22:43:21 -0300

    [NEW] Modulo 11

--- src/erpbrasil/base/misc.py
+++ src/erpbrasil/base/misc.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 # Copyright (C) 2014  Renato Lima - Akretion
 # Copyright (C) 2015  Luis Felipe Mileo - KMEE
+# Copyright (C) 2015  Base4 Sistemas Ltda ME
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 import re
@@ -27,3 +28,19 @@ def format_zipcode(zipcode, country_code='BR'):
         if len(val) == 8:
             zipcode_formatted = "%s-%s" % (val[0:5], val[5:8])
     return zipcode_formatted
+
+
+def modulo11(base):
+    """Calcula o dígito verificador (DV) para o argumento usando "Módulo 11".
+    :param str base: String contendo os dígitos sobre os quais o DV será
+        calculado, assumindo que o DV não está incluído no argumento.
+    :return: O dígito verificador calculado.
+
+    Source: https://github.com/base4sistemas/satcomum/blob/f45da5b100a63511b9c455cbd6895b630e121866/satcomum/util.py
+
+    :rtype: int
+    """
+    pesos = '23456789' * ((len(base) // 8) + 1)
+    acumulado = sum([int(a) * int(b) for a, b in zip(base[::-1], pesos)])
+    digito = 11 - (acumulado % 11)
+    return 0 if digito >= 10 else digito
