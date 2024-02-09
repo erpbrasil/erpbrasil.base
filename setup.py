c486174e3a1477e812efb04f199674711d393891
Magno Costa <magno.costa@akretion.com.br>
2024-02-09 16:29:19 -0300

    [UPD] Atualização Biblioteca para o padrão atual do Python, incluído o arquivo pyproject.toml

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import print_function
-
-import io
-import re
-from glob import glob
-from os.path import basename
-from os.path import dirname
-from os.path import join
-from os.path import splitext
-
-from setuptools import find_packages
 from setuptools import setup
 
 
