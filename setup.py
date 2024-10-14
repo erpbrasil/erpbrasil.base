a0cd735cd817a120d45d4c096b90c0105de8b89c
Luis Felipe Miléo <mileo@kmee.com.br>
2024-10-14 17:29:21 -0300

    Bump version: 2.3.0 → 2.3.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name="erpbrasil.base",
-    version="2.3.0",
+    version="2.3.1",
     license="MIT license",
     description="Base",
     long_description="%s\n%s"
